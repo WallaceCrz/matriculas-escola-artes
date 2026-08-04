2d0b767bd3754a97470707ef3fbedb02c0aca7a2
WallaceCrz <92836149+WallaceCrz@users.noreply.github.com>
2026-08-04 15:53:08 -0300

    Duplicados matriculas

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ export const CONFIG = {
   ANO_SEMESTRE_DEFAULT: '2026.2',
 
   // Única fonte da URL do Web App. Atualize este valor após implantar o Apps Script.
-  DEFAULT_APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxg5Ou87A7rKbb1CLg4NaBuHYzphxyoc38NNoewnlZ4jr_sGU1kFDvKmOXMor53-EHGhg/exec',
+  DEFAULT_APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbx83bhxBT0s7VFcQsvnHZzqlJAbJmip8HmGVNaAXKQ8yd-nYhqSuS9nW9vyx4eoqRUH5A/exec',
 
   // Administrador local, mantido no código conforme solicitado.
   ADMIN_LOGIN: 'admin',
